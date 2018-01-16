6b0dfc42d2f200adca251499776a0b5d12bc77ca
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 10:46:09 -0600

    WIP; Importing Router, Route and Switch to create a routing system. When the urls match '/', /dinners' , or '/dinners/new the Switch component will cause the associated component to be loaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import SideNavBar from './components/nav/SideNavBar';
 import './App.css';
@@ -6,14 +7,25 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <SideNavBar />
-        </p>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+        </div>
+        <Router>
+          <div>
+            <p className="App-intro">
+              <SideNavBar />
+            </p>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/dinners/new' component={DinnerForm} />
+              <Route path='/dinners' component={Dinners} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
