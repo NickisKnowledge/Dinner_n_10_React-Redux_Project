12e28cb4bbdeaeb31ee35fc63e2a381da31650b7
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 10:01:59 -0600

    WIP; Created the dinners reducer

---
+++ client/src/reducers/dinnersReducer.js
@@ -0,0 +1,20 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_DINNER': {
+      return state.concat(action.dinner);
+    }
+
+    case 'REMOVE_DINNER': {
+      const idx = state.findIndex(dinner => dinner.id === action.dinnerId);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+    }
+
+    case 'SET_DINNERS': {
+      return action.dinners;
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
