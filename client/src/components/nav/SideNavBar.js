3ea1a5eca613ba6e800578ac6b671b8cd50f89a3
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 10:32:21 -0600

    WIP; Created a side navbar component, that will serve as the navigation for site

---
+++ client/src/components/nav/SideNavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SideNavBar = () => {
+  return (
+    <div>
+      <a href='/'>Home</a>
+      <a href='/dinners'>Dinners</a>
+      <a href='/dinners/new'>New Dinner</a>
+    </div>
+  );
+};
+
+export default SideNavBar;
