4c3606ee21e3b07d3073e6df9c62ab63437118de
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-15 22:08:34 -0600

    WIP; Added 'accepts_nested_attributes_for' to create attribute writers for each of Dinners associated models, will be sending one form to api that has values for each model. ':allow_destroy => true' will delete all the dinners associated attributes when/if it's deleted

--- app/models/dinner.rb
+++ app/models/dinner.rb
@@ -3,5 +3,9 @@ class Dinner < ApplicationRecord
   has_many :directions
   has_one :image
 
+  accepts_nested_attributes_for :image, :allow_destroy => true
+  accepts_nested_attributes_for :ingredients, :allow_destroy => true
+  accepts_nested_attributes_for :directions, :allow_destroy => true
+
   validates_presence_of :title, :image, :ingredients, :directions
 end
