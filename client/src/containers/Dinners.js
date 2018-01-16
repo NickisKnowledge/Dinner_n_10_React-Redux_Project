c22a8585b937eda0aab7f83dfbaf174c2eed3cf6
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 10:59:30 -0600

    WIP; Created skeleton Dinners component, this will serve as the index page

---
+++ client/src/containers/Dinners.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Dinners extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Dinners Show Page</h1>
+      </div>    )
+  }
+};
+
+export default Dinners;
