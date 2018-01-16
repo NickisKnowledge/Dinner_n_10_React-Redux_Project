b90953281f013c0fc7433c49e54f8a50de2ae4b4
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-15 21:35:27 -0600

    WIP; Created spec directory, configured rails helper to use DatabaseCleaner and Shoulda::Matchers. Added Dinner model tests to check associations and validations of associated models

--- spec/models/dinner_spec.rb
+++ spec/models/dinner_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Dinner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:directions) }
+  it { should have_one(:image) }
+  it { should have_many(:ingredients)}
+
+  it "should validate the presence of it's title" do
+    should validate_presence_of(:title)
+  end
+
+  it "should validate the presence of it's associated model Direction" do
+    should validate_presence_of(:directions)
+  end
+
+  it "should validate the presence of it's associated model Image" do
+    should validate_presence_of(:image)
+  end
+
+  it "should validate the presence of it's associated model Ingredient" do
+    should validate_presence_of(:ingredients)
+  end
 end
