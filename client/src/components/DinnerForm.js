66ed0273fb87d4c568d106e59a0c6a4ad59a2fda
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 16:20:22 -0600

    WIP; Creating the Dinner form, set up the state and have one input for the name of the dinner. Will update states title with user input with the function 'handleTitleInput'

--- client/src/components/DinnerForm.js
+++ client/src/components/DinnerForm.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class DinnerForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      ingredients_attributes: [{ name: '' }],
+      directions_attributes: [{ step: '' }],
+      image_attributes: {url: '', name: ''},
+    };
+  }
+
   render() {
     return(
       <div>
-        <h1>Dinner Form</h1>
+        <h1>Add your Recipe</h1>
+        <hr className='headerDivider' />
+        <form>
+          <div>
+          <label>Dinner Name: </label>
+            <input
+              type="text"
+              value={this.state.title}
+              onChange={this.handleTitleInput}
+            />
+          </div>
+        </form>
       </div>
     );
   }
