fef2f4473f9711f8461d92816ee0a6441e2fd22c
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 13:22:10 -0600

    WIP; Added Home page welcome info

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+    <h1>Welcome</h1>
+    <p>
+      Being a single mom and not liking to cook hasn't worked out very well.
+      Dinner-n-10 is my solution! Most of these meals are in small portion sizes
+      because I only have to prepare them for a small child and myself, nevertheless,
+      their delicious and take AT MOST 10 steps to create.
+    </p>
     </div>
   );
 };
