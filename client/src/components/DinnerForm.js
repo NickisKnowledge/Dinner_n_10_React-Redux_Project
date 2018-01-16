c6cb7af96f139deb8e3d1deb6967874a64ea118a
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 10:57:44 -0600

    WIP; Created skeleton Home and DinnerForm components

---
+++ client/src/components/DinnerForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DinnerForm extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Dinner Form</h1>
+      </div>
+    );
+  }
+};
+
+export default DinnerForm;
