627181dfbb7cba4097df21e6754794e6a83e3b69
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-15 21:49:34 -0600

    WIP; Added test to check that Direction belongs to dinner and that the step attribute is required

--- spec/models/direction_spec.rb
+++ spec/models/direction_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Direction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:dinner) }
+  
+  it "should validate the presence of it's step attribute" do
+     should validate_presence_of(:step)
+  end
 end
