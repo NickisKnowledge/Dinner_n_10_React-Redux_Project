6409b514bbef0c9367bd0d342dd0167fc1e66b53
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-17 00:11:01 -0600

    WIP; Made DinnerList component to format the index page

---
+++ client/src/components/DinnerList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Image from './Image';
+
+const DinnerList = ({ dinners, url }) => {
+  const renderDinners = dinners.map(dinner =>
+    <div key={dinner.id}>
+      <h2>{dinner.title}</h2>
+      <Link
+        to={`${url}/${dinner.id}`}
+        style={{ textDecoration: 'none' }}
+      >
+        <Image
+          imageURL={dinner.image.url}
+          imageName={dinner.image.name}
+          style={{width: 250, height: 250}}
+        />
+      </Link>
+      <hr className='headerDivider' />
+    </div>
+  );
+
+  return (
+    <div>
+      {renderDinners}
+    </div>
+  );
+};
+
+export default DinnerList;
