807c10d9e0eca117e27a8af8e8e5d542d47acde6
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 23:59:13 -0600

    WIP; Created the Dinners container to test the fetchDinners function

--- client/src/containers/Dinners.js
+++ client/src/containers/Dinners.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchDinners } from '../actions/dinners';
+
+const mapStateToProps = state => { return { dinners: state.dinners } };
 
 class Dinners extends Component {
+  componentDidMount() {
+    this.props.fetchDinners();
+  }
+
   render() {
-    return(
-      <div>
-        <h1>Dinners Show Page</h1>
-      </div>    )
+    const {dinners, match} = this.props;
+    const showDinners = dinners.map(dinner => {
+      return (
+        <div>
+          <Link
+          to={`${match.url}/${dinner.id}`}
+          key={dinner.id}
+          style={{ textDecoration: 'none' }}
+          >
+            <h2>{dinner.title}</h2>
+          </Link>
+          <hr className='headerDivider' />
+        </div>
+      )
+     }
+     );
+
+    return (
+      <div class='container'>
+        <h1>Bon Appétit</h1>
+        <hr/>
+        {showDinners}
+      </div>
+    )
   }
 };
 
-export default Dinners;
+export default connect(mapStateToProps, { fetchDinners })(Dinners);
