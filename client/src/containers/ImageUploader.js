61342b8737d1915c9b9e4ce5b71514ef25a6290d
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-16 20:27:51 -0600

    WIP; Created parameters hash needed to upload images to my cloudinary account, imported sh1 to encrypt my signature

--- client/src/containers/ImageUploader.js
+++ client/src/containers/ImageUploader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
+import sha1 from 'sha1';
 
 class ImageUploader extends Component {
   constructor(props) {
@@ -12,7 +13,24 @@ class ImageUploader extends Component {
   }
 
   uploadFile = (files) => {
-    console.log(files);
+    const image = files[0];
+    const cloudName = 'nickisknowledge'
+    const url = 'https://api.cloudinary.com/v1_1/'+cloudName+'/image/upload'
+    const timestamp = Date.now()/1000
+    const uploadPreset = 'fyxfsz9r'
+
+    const paramsStr = 'timestamp='+timestamp+'&upload_preset='+uploadPreset+
+    'l53JMlRfRSBrfqbhzWZzfwpLH8Y'
+
+    const signature = sha1(paramsStr)
+
+    const params = {
+      'api_key': '192174765614618',
+      'timestamp': timestamp,
+      'upload_preset': uploadPreset,
+      'signature': signature,
+    }
+
   };
 
   render() {
