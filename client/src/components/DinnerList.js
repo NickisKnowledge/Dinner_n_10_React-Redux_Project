9be5c5bd00ce391710556d785a1b194a2e0bc4c6
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-24 20:03:04 -0600

    WIP; Extracted dinner list markup to DinnerLi component, import component and pass in the dinner and url

--- client/src/components/DinnerList.js
+++ client/src/components/DinnerList.js
@@ -1,32 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Image from './Image';
+import React, { Component } from 'react';
+import DinnerLi from './DinnerLi';
 
-const DinnerList = ({ dinners, url }) => {
-  const renderDinners = dinners.map(dinner =>
-    <div>
-      <div key={dinner.id} className='listDinners'>
-        <h2 className='dinnerIndexTitle'>{dinner.title}</h2>
-        <Link
-          to={`${url}/${dinner.id}`}
-          style={{ textDecoration: 'none' }}
-        >
-          <Image
-            imageURL={dinner.image.url}
-            imageName={dinner.image.name}
-            style={{width: 250, height: 250}}
-          />
-        </Link>
+class DinnerList extends Component {
+
+  render() {
+    const { dinners, url } = this.props;
+
+    const renderDinners = dinners.map(dinner =>
+      <DinnerLi dinner={dinner} url={url} key={dinner.id} />
+    );
+
+    return (
+      <div>
+        {renderDinners}
       </div>
-      <hr className='headerDivider' />
-    </div>
-  );
-
-  return (
-    <div>
-      {renderDinners}
-    </div>
-  );
+    );
+  };
 };
 
 export default DinnerList;
