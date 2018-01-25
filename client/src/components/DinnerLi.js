0d94bd53d91acd06fd5ac9f4fd28fbd60843889b
Nickis'Knowledge <lifechaningstuff@gmail.com>
2018-01-24 19:59:14 -0600

    WIP; ​DinnerLi is an added component from Project review. Added functionality to be able to like a dinner​ and show it's number of likes -- NOT persisted

---
+++ client/src/components/DinnerLi.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Image from './Image';
+
+class DinnerLi extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      votes: 0
+    };
+  }
+
+  handleClick = () => {
+    let currentVotes = this.state.votes
+    this.setState({
+      votes:  currentVotes += 1
+    });
+  };
+
+  render() {
+    const { dinner, url } = this.props;
+
+    return(
+      <div>
+        <div className='listDinners'>
+          <h2 className='dinnerIndexTitle'>{dinner.title}</h2>
+          <Link
+            to={`${url}/${dinner.id}`}
+            style={{ textDecoration: 'none' }}
+          >
+            <Image
+              imageURL={dinner.image.url}
+              imageName={dinner.image.name}
+              style={{width: 250, height: 250}}
+            />
+          </Link>
+          <input type='button' value='Like me' onClick={this.handleClick.bind(this) } />
+          <div>
+            {this.state.votes}
+          </div>
+        </div>
+        <hr className='headerDivider' />
+      </div>
+    )
+  }
+};
+
+export default DinnerLi;
