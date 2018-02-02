fd077ee62666ccb01e7d0a881295781ed6edba14
NickisKnowledge <lifechangingstuff@gmail.com>
2018-02-02 16:43:55 -0600

    WIP; Display dinners based on number of likes

--- client/src/components/DinnerList.js
+++ client/src/components/DinnerList.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import DinnerLi from './DinnerLi';
 
 class DinnerList extends Component {
-
+  sortedByRating = dinners =>
+    dinners.sort((a, b) => {
+      let aRating = a.rating;
+      let bRating = b.rating;
+      // b - a, so the dinner with the most likes appears first
+      return bRating - aRating;
+    });
+  
   render() {
     const { dinners, url } = this.props;
-
-    const renderDinners = dinners.map(dinner =>
+    const sortedDinners = this.sortedByRating(dinners);
+    const renderDinners = sortedDinners.map(dinner =>
       <DinnerLi dinner={dinner} url={url} key={dinner.id} />
     );
 
