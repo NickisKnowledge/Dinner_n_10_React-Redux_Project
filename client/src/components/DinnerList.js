eb9adec4df75143baea37087a3f30de2ecfc20e3
NickisKnowledge <lifechangingstuff@gmail.com>
2018-02-15 23:00:13 -0600

    WIP; Created a form which will eventually allow the user to filter dinners via the  search word. For now, a users input updates the state

--- client/src/components/DinnerList.js
+++ client/src/components/DinnerList.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import DinnerLi from './DinnerLi';
 
 class DinnerList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchWord: '',
+      selectedDinners: []
+    }
+  }
+
   sortedByRating = dinners =>
     dinners.sort((a, b) => {
       let aRating = a.rating;
@@ -9,7 +18,10 @@ class DinnerList extends Component {
       // b - a, so the dinner with the most likes appears first
       return bRating - aRating;
     });
-  
+
+    handleInput = event => {
+      this.setState({ searchWord: event.target.value });
+    }
   render() {
     const { dinners, url } = this.props;
     const sortedDinners = this.sortedByRating(dinners);
@@ -19,6 +31,15 @@ class DinnerList extends Component {
 
     return (
       <div>
+        <div style={{ float: 'right' }}>
+          <label>Keyword Search </label><br />
+          <input
+            type='text'
+            value={this.state.searchWord}
+            onChange={this.handleInput}
+          />< br />
+          <input type='button' value='Search' />
+        </div>
         {renderDinners}
       </div>
     );
